6774fffe9b99ecab1341b085f3e486de821fab78
Nicolas MURE <nicolas.mure@knplabs.com>
2018-06-29 16:14:10 +0200

    fix bad rebase

--- src/lib.rs
+++ src/lib.rs
@@ -133,10 +133,6 @@ pub extern fn update_state(elapsed_time: f32) {
         Some(x) => state.lifepacks.push(x),
         None => {}
     }
-
-    let stage_width: u16 = state.width;
-    let stage_height: u16 = state.height;
-    move_enemies(&mut state.enemies, elapsed_time, stage_width, stage_height);
 }
 
 #[wasm_bindgen]
